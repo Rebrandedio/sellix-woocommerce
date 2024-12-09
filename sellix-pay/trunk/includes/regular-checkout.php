218c3e1fb1551794a848b49af785aa8cad0dc183
Donnie R. Battle Jr <dbattle7500@gmail.com>
2024-12-09 00:54:40 -0500

    minimize code changes by adding back commas

--- sellix-pay/trunk/includes/regular-checkout.php
+++ sellix-pay/trunk/includes/regular-checkout.php
@@ -112,7 +112,7 @@ class WC_Gateway_SellixPay extends WC_Payment_Gateway
 				'description' => __('If you have more than one shop (merchant) under your Sellix account, you can send API requests with their authorization by passing theX-Sellix-Merchant header to each request.', 'sellix-pay').' '.
 				'For example if your Sellix account has two merchants (1. Jack, 2. James) and you want to make API requests as James, you need to pass the X-Sellix-Merchant header with value James to able to authenticate as different stores',
 				'default' => '',
-			]
+			],
 		];
 	}
 
@@ -126,7 +126,7 @@ class WC_Gateway_SellixPay extends WC_Payment_Gateway
 			'webhook' => add_query_arg('wc_id', $order->get_id(), $this->webhook_url),
 			'email' => $order->get_billing_email(),
 			'value' => $order->get_total(),
-			'origin' => 'WOOCOMMERCE'
+			'origin' => 'WOOCOMMERCE',
 		];
 
 		$route = "/v1/payments";
@@ -286,6 +286,6 @@ class WC_Gateway_SellixPay extends WC_Payment_Gateway
 	public function complete_order($wc_id) {
 		global $woocommerce;
 		$order = wc_get_order($wc_id);
-		$order->update_status('processing', __('Payment received and processing', 'sellix-pay'));
+		$order->update_status('processing');
 	}
 }
